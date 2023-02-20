f178023615ba4b11606c0560dcbf516416adb8ae
Valery-Lytvyn <lytvynvalery@gmail.com>
2023-02-20 12:27:35 +0200

    refactored something css code &  created pagination and localStorage

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 "use strict";
 let serverResponse;
+let storySearchQuery;
+let storyRadioButton;
+let storyCurrentPage;
 let searchResults = document.querySelector(".searchResults");
 let failResultMessage = document.createElement("div");
+let radioButton
 let moviesList;
 let numberPagesWrapper;
+let visiblyPagesList = [];
 let totalNumberPages;
 let nextPage;
 let currentPage = 1;
 let movieDetails;
-let story;
+let storyMoviesList;
 
 let clearButton = document.querySelector(".clearButton");
 clearButton.after(searchResults);
@@ -17,36 +22,28 @@ clearButton.addEventListener("click", function () {
    removeResult();
 });
 
-function getLocalStorage() {
-   if (localStorage.length > 0 && !localStorage.getItem("failResultMessage")) {
-      story = JSON.parse(localStorage.getItem("moviesList"));
-      createMoviesList(story);
-   } else
-      if (localStorage.getItem("failResultMessage")) {
-         showFailResultMessage();
-      }
-}
-
-getLocalStorage()
-
-
 let searchButton = document.getElementById("searchButton");
-searchButton.addEventListener("click", serverRequest);
-
+searchButton.addEventListener("click", function (event) {
+   serverRequest(currentPage);
+});
 
 let inputForm = document.forms.form;
 let searchInput = inputForm.searchInput;
 searchInput.addEventListener("keydown", function (event) {
    if (event.key === "Enter") {
+      setLocalStorage("storyCurrentPage", 1);
+      currentPage = 1;
       event.preventDefault();
-      serverRequest();
+      serverRequest(currentPage);
    }
 });
 
-let radioButton = null;
+radioButton = null;
 let radioForm = document.forms.radioSection;
 for (let i = 0; i < radioForm.length; i++) {
    radioForm[i].addEventListener("change", function () {
+      setLocalStorage("storyCurrentPage", 1);
+      currentPage = 1;
       radioButton = this.value;
    });
 }
@@ -57,13 +54,33 @@ let status = function (response) {
       return Promise.reject(new Error(response.statusText));
    }
    return Promise.resolve(response);
-};
+}
+
 let json = function (response) {
    return response.json();
-};
+}
+
+function getStorageParse(keyName) {
+   return JSON.parse(localStorage.getItem(keyName));
+}
+
+function getLocalStorage() {
+   if (localStorage.getItem("storySearchQuery")) {
+      storySearchQuery = getStorageParse("storySearchQuery");
+      storyRadioButton = getStorageParse("storyRadioButton");
+      storyCurrentPage = getStorageParse("storyCurrentPage");
+      searchInput.value = storySearchQuery;
+      currentPage = storyCurrentPage;
+      serverRequest(currentPage);
+   }
+}
+getLocalStorage()
 
 function serverRequest(currentPage) {
    let searchQuery = searchInput.value;
+   if (searchQuery.length < 3) {
+      alert("Enter at least 3 characters");
+   };
    if (searchQuery) {
       fetch(
          `https://www.omdbapi.com/?s=${searchQuery}&type=${radioButton}&page=${currentPage}&apikey=465e935e&`
@@ -71,16 +88,17 @@ function serverRequest(currentPage) {
          .then(status)
          .then(json)
          .then(function (response) {
+            setLocalStorage("storySearchQuery", searchQuery)
+            setLocalStorage("storyRadioButton", radioButton)
+            setLocalStorage("storyCurrentPage", currentPage)
             removeResult();
             if (!response.Search) {
                showFailResultMessage();
-               setLocalStorage("failResultMessage", "Movie not found!");
             } else {
                serverResponse = response.Search;
                createMoviesList(serverResponse);
-               setLocalStorage("moviesList", serverResponse);
                if (response.totalResults > 10) {
-                  createListPages(response.totalResults)
+                  pagination(response.totalResults);
                }
             }
          });
@@ -90,13 +108,10 @@ function serverRequest(currentPage) {
 function showFailResultMessage() {
    failResultMessage.style.color = "red";
    failResultMessage.innerHTML = "Movie not found!";
-   elementPosition(searchResults, failResultMessage, 'prepend')
+   elementPosition(searchResults, failResultMessage, "prepend");
 }
 
-
-
 function setLocalStorage(keyName, keyValue) {
-   localStorage.clear();
    localStorage.setItem(keyName, JSON.stringify(keyValue));
 }
 
@@ -105,72 +120,88 @@ function createMoviesList(arr) {
    moviesList.addEventListener("click", clickDetailsMovie);
 
    arr.forEach((item, index) => {
-      elementPosition(searchResults, moviesList, 'prepend')
+      elementPosition(searchResults, moviesList, "prepend")
       let liWrap = document.createElement("div");
-      elementPosition(moviesList, liWrap, 'append')
+      elementPosition(moviesList, liWrap, "append")
       let li = document.createElement("li");
       let sequenceNumber = (currentPage - 1) * 10 + 1 + index;
       li.innerText = `${sequenceNumber}. ${item.Title}`;
-      elementPosition(liWrap, li, 'prepend')
-
-      movieDetails = document.createElement("button");
-      movieDetails.classList.add("btn");
-      movieDetails.id = `id = ${index}`;
-      movieDetails.innerHTML = "DETAILS";
-      li.after(movieDetails);
-      elementPosition(li, movieDetails, 'after')
-
+      elementPosition(liWrap, li, "prepend")
+      let movieDetailsButton = document.createElement("button");
+      movieDetailsButton.classList.add("btn");
+      movieDetailsButton.id = `id = ${index}`;
+      movieDetailsButton.innerHTML = "DETAILS";
+      elementPosition(li, movieDetailsButton, "after")
    });
 }
 
 function createPagesSection(number) {
    numberPagesWrapper = document.createElement("div");
-   numberPagesWrapper.classList.add('numberPagesWrapper')
-   elementPosition(searchResults, numberPagesWrapper, 'after')
+   numberPagesWrapper.classList.add("numberPagesWrapper")
+   elementPosition(searchResults, numberPagesWrapper, "after")
    numberPagesWrapper.addEventListener("click", changePage);
 
    let previousPage = document.createElement("div");
    previousPage.classList.add("btn");
-   elementPosition(numberPagesWrapper, previousPage, 'prepend')
+   elementPosition(numberPagesWrapper, previousPage, "prepend")
    previousPage.innerHTML = "pre page";
    previousPage.id = "-1";
 
    nextPage = document.createElement("div");
    nextPage.classList.add("btn");
-   elementPosition(numberPagesWrapper, nextPage, 'append')
+   elementPosition(numberPagesWrapper, nextPage, "append")
    nextPage.innerHTML = "next page";
    nextPage.id = "-2";
 }
 
-function createListPages(number) {
+function createVisiblyPagesList(totalNumberPages) {
+   visiblyPagesList = []
+   for (let i = 0; i <= 3; i++) {
+      visiblyPagesList.push(i, totalNumberPages - i, currentPage - i, currentPage + i);
+   }
+}
+
+
+
+function pagination(number) {
    createPagesSection();
    totalNumberPages = Math.ceil(number * 0.1);
-   for (let i = 1; i <= totalNumberPages; i++) {
-      let page = document.createElement("div");
-      page.classList.add("btn")
-      elementPosition(nextPage, page, 'before')
-      page.innerHTML = i;
-      page.id = i;
-      if (i === currentPage) {
-         page.classList.add('active')
+   createVisiblyPagesList(totalNumberPages)
+   let tempPrevPage = 0;
+   for (let page = 1; page <= totalNumberPages; page++) {
+      if (visiblyPagesList.includes(page)) {
+         if ((page - tempPrevPage) != 1) {
+            let threeDots = document.createElement("div");
+            threeDots.classList.add("btn")
+            elementPosition(nextPage, threeDots, "before")
+            threeDots.innerHTML = "...";
+         }
+         let pageNumber = document.createElement("div");
+         pageNumber.classList.add("btn")
+         elementPosition(nextPage, pageNumber, "before")
+         pageNumber.innerHTML = page;
+         pageNumber.id = page;
+         tempPrevPage = page;
+         if (page === currentPage) {
+            pageNumber.classList.add("active")
+         }
       }
    }
 }
 
 function changePage(event) {
-   let idButton = Number(event.target.id);
-   if ((idButton === -1 && currentPage === 1) ||
-      (idButton === -2 && currentPage === totalNumberPages)) {
+   let idPage = Number(event.target.id);
+   if ((idPage === -1 && currentPage === 1) ||
+      (idPage === -2 && currentPage === totalNumberPages) || (!idPage)) {
    } else
-      if (idButton === -1 && currentPage > 1) {
-         currentPage = currentPage - 1;
+      if (idPage === -1 && currentPage > 1) {
+         currentPage = --currentPage;
          serverRequest(currentPage);
-      } else if (idButton === -2 && currentPage < totalNumberPages) {
-         currentPage = currentPage + 1;
+      } else if (idPage === -2 && currentPage < totalNumberPages) {
+         currentPage = ++currentPage;
          serverRequest(currentPage);
       } else {
-         removeResult();
-         currentPage = idButton;
+         currentPage = idPage;
          serverRequest(currentPage);
       }
 }
@@ -178,7 +209,7 @@ function changePage(event) {
 function clickDetailsMovie(event) {
    if (event.target.tagName === "BUTTON") {
       if (!serverResponse) {
-         serverResponse = story;
+         serverResponse = storyMoviesList;
       }
       createListDetailsMovie(event)
       setTimeout(() => searchResults.scrollIntoView());
@@ -196,42 +227,42 @@ function createListDetailsMovie(event) {
    showDetailsMovie(titleMovie, typeMovie, yearMovie, posterMovie);
 }
 
-
 function showDetailsMovie(title, type, year, poster) {
-   if (movieDetails) {
-      movieDetails.remove();
-   }
-   movieDetails = document.createElement("div");
-   movieDetails.classList.add('movieDetails');
+   numberPagesWrapper.style.display = "none";
    let movieDetailsWrap = document.createElement("div");
-   movieDetailsWrap.classList.add('movieDetailsWrap');
-   elementPosition(searchResults, movieDetailsWrap, 'prepend')
-   elementPosition(movieDetailsWrap, movieDetails, 'prepend')
+   movieDetailsWrap.classList.add("movieDetailsWrap");
+   elementPosition(searchResults, movieDetailsWrap, "prepend")
+   movieDetails = document.createElement("div");
+   movieDetails.classList.add("movieDetails");
+   elementPosition(movieDetailsWrap, movieDetails, "prepend")
 
    let closeDiv = document.createElement("div");
-   closeDiv.classList.add('button');
+   closeDiv.classList.add("button");
    let closeDivText = document.createElement("div")
-   elementPosition(closeDiv, closeDivText, 'append')
-   closeDivText.classList.add('buttonText');
+   elementPosition(closeDiv, closeDivText, "append")
+   closeDivText.classList.add("buttonText");
    closeDivText.innerHTML = `close`;
-   elementPosition(movieDetailsWrap, closeDiv, 'prepend')
-   closeDiv.onclick = () => movieDetailsWrap.remove();
+   elementPosition(movieDetailsWrap, closeDiv, "prepend")
+   closeDiv.onclick = () => {
+      movieDetailsWrap.remove();
+      numberPagesWrapper.style.display = "flex";
+   }
    let titleDiv = document.createElement("div");
    titleDiv.innerHTML = `Title: ${title}`;
    titleDiv.style.padding = "15px";
-   elementPosition(movieDetails, titleDiv, 'append')
+   elementPosition(movieDetails, titleDiv, "append")
    let typeDiv = document.createElement("div");
    typeDiv.innerHTML = `Type: ${type}`;
    typeDiv.style.padding = "15px";
-   elementPosition(movieDetails, typeDiv, 'append')
+   elementPosition(movieDetails, typeDiv, "append")
    let yearDiv = document.createElement("div");
    yearDiv.innerHTML = `Year: ${year}`;
    yearDiv.style.padding = "15px";
-   elementPosition(movieDetails, yearDiv, 'append')
+   elementPosition(movieDetails, yearDiv, "append")
    if (poster != "N/A") {
       let posterImg = document.createElement("img");
       posterImg.src = poster;
-      elementPosition(movieDetails, posterImg, 'append')
+      elementPosition(movieDetails, posterImg, "append")
       posterImg.style.padding = "15px";
    }
 }
@@ -253,13 +284,13 @@ function removeResult() {
 
 function elementPosition(parentElement, currentElement, position) {
    switch (position) {
-      case 'prepend':
+      case "prepend":
          return (parentElement.prepend(currentElement))
-      case 'append':
+      case "append":
          return (parentElement.append(currentElement))
-      case 'after':
+      case "after":
          return (parentElement.after(currentElement))
-      case 'before':
+      case "before":
          return (parentElement.before(currentElement))
    }
 }
